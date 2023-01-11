30ef9f764d82e25ac435210f4b3b7a846f15231e
Josh Burd <Jburdt@gmail.com>
2023-01-11 17:44:04 -0500

    added columns to tools table

--- db/migrate/20230111224054_create_tools.rb
+++ db/migrate/20230111224054_create_tools.rb
@@ -1,4 +1,11 @@
 class CreateTools < ActiveRecord::Migration[6.1]
   def change
+    create_table :tools do |t|
+      t.string :title
+      t.integer :price
+      t.integer :hours_needed
+      t.string :description 
+      t.timstamps
+    end
   end
 end
