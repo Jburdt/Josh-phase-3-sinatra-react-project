a173a10cfba692bc464bc0ac5d1c844bf72e0a53
Josh Burd <Jburdt@gmail.com>
2023-01-11 17:45:39 -0500

    added columns to contractors table

--- db/migrate/20230111224217_create_contractors.rb
+++ db/migrate/20230111224217_create_contractors.rb
@@ -1,4 +1,10 @@
 class CreateContractors < ActiveRecord::Migration[6.1]
   def change
+    create_table :contractors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_number
+      t.string :company
+    end
   end
 end
