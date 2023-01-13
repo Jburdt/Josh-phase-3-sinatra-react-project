4c9eaf4cdd115619b63845d02bed33b4d88f5154
Josh Burd <Jburdt@gmail.com>
2023-01-13 13:31:18 -0500

    Added navbar

---
+++ my-app-frontend/src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header>
+      <h3>Logo</h3>\
+      <nav>
+          <a href='/#'>Home</a>
+          <a href='/#'>Tools</a>
+          <a href='/#'>Contractors</a>
+          <a href='/#'>Rented</a>
+      </nav>
+    </header>
+  )
+}
+
+export default NavBar
