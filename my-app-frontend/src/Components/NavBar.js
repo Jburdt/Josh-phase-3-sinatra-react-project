2991f10221d4a4508def1cbd9cf5b1e967928de3
Josh Burd <Jburdt@gmail.com>
2023-01-13 13:59:11 -0500

    added onclicks to nav btn

--- my-app-frontend/src/Components/NavBar.js
+++ my-app-frontend/src/Components/NavBar.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from 'react';
 
 const NavBar = () => {
+
+  const navRef = useRef();
+
+  const showNavBar = () => {
+    navRef.current.classlist.toggle('responsive_nav');
+  }
+
   return (
     <header>
       <h3>Logo</h3>
       <nav>
-          <a href='/#'>Home</a>
+          <a href={navRef}>Home</a>
           <a href='/#'>Tools</a>
           <a href='/#'>Contractors</a>
           <a href='/#'>Rented</a>
-          <button>
+          <button className="nav-btn nav-close-btn" onClick={showNavBar}>
             <FaTimes/>
           </button>
       </nav>
-      <button>
+      <button className="nav-btn" onClick={showNavBar}>
         <FaBars />
       </button>
     </header>
   )
 }
 
-export default NavBar
+export default NavBar;
