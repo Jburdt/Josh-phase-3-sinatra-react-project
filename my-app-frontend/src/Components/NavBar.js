676a6e51eaaadc0806e6601003361a035c008713
Josh Burd <Jburdt@gmail.com>
2023-01-13 13:44:56 -0500

    Added buttons to navbar

--- my-app-frontend/src/Components/NavBar.js
+++ my-app-frontend/src/Components/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
   return (
     <header>
-      <h3>Logo</h3>\
+      <h3>Logo</h3>
       <nav>
           <a href='/#'>Home</a>
           <a href='/#'>Tools</a>
           <a href='/#'>Contractors</a>
           <a href='/#'>Rented</a>
+          <button>
+            <FaTimes/>
+          </button>
       </nav>
+      <button>
+        <FaBars />
+      </button>
     </header>
   )
 }
