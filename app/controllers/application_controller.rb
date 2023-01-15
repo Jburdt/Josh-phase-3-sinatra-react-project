37d11511ab29ab0edb0b8f458d4c2dd15a32cffd
Josh Burd <Jburdt@gmail.com>
2023-01-14 22:40:36 -0500

    added get and patch method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,23 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project sir!" }.to_json
+  post "/tools" do
+    tools = Tool.create(name: params[:item], price: params[:price], description: params[:description], condition: params[:condition])
+    tools.to_json( include: { tool: {only: [:tool]} })
   end
+
+  get "/tools" do 
+    tools = Tool.all.order(:name)
+    tools.to_json( include: { tool: {only: [:tool]} })
+  end
+
+  patch "/tools/:id" do 
+    tools = Tools.find(params[:id])
+    tools.update(name: params[:name])
+    tools.to_json( include: { tool: {only: [:tool]} })
+  end
+
+  
+    
 
 end
